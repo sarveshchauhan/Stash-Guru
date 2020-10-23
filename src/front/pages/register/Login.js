8315fa5cb99ee40ec4d1dea1a67fb44763b02449
sanju2121 <69622591+sanju2121@users.noreply.github.com>
2020-10-23 13:49:38 +0530

    login_update

--- src/front/pages/register/Login.js
+++ src/front/pages/register/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Col, Container, Row ,Form} from 'react-bootstrap';
 import GoogleLogo from '../../../assets/front/images/icons/GoogleLogo.png';
 import facebook from '../../../assets/front/images/icons/facebook.png';
@@ -8,85 +8,114 @@ import family from '../../../assets/front/images/img/family.svg';
 
 import './Register.scss';
 import { NavLink } from 'react-router-dom';
+// import { createStore } from 'redux';
+// import authApp from '../../../redux';
+// const store = createStore(authApp)
 
+// function LoginComponentCtrl(){
+class LoginComponentCtrl extends React.Component{
 
-function LoginComponentCtrl(){
-    return(
-        <>
-        <section className="section_padding">
-            <Container>
-                <Row className="align-items-center">
-                    <Col sm={4} className="my-2">
-                        <img src={catoons} />
-                    </Col>
-                    <Col sm={4} className="my-2">
-                        <div className="register_card">
-                            <Row>
-                                <Col sm={12}>
-                                    <div>
-                                        <h2 className="register_hdng">
-                                            <span>Login</span> To Continue
-                                        </h2>
-                                    </div>
-                                </Col>
-                                <Col sm={12}>
-                                        <Form>
-                                            <Row className="my-2 pt-2">
-                                                <Col>
-                                                    <Form.Group controlId="">
-                                                        <Form.Control type="email" placeholder="Email Address" />
-                                                    </Form.Group>
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
 
-                                                    <Form.Group controlId="">
-                                                        <Form.Control type="password" placeholder="Password" />
-                                                    </Form.Group>
-                                                </Col>
-                                            </Row>
+    handleSubmit(event) {
+        console.log('A name was submitted: ' + this.state.email);
+        console.log('A name was submitted: ' + this.state.password);
+        event.preventDefault();
+    }
+
+    render() {
+        return(
+            <>
+            <section className="section_padding">
+                <Container>
+                    <Row className="align-items-center">
+                        <Col sm={4} className="my-2">
+                            <img src={catoons} />
+                        </Col>
+                        <Col sm={4} className="my-2">
+                            <div className="register_card">
+                                <Row>
+                                    <Col sm={12}>
+                                        <div>
+                                            <h2 className="register_hdng">
+                                                <span>Login</span> To Continue
+                                            </h2>
+                                        </div>
+                                    </Col>
+                                    <Col sm={12}>
+                                            <Form onSubmit={this.handleSubmit}>
+                                                <Row className="my-2 pt-2">
+                                                    <Col>
+                                                        <Form.Group controlId="">
+                                                            <Form.Control type="email" name="email" value={this.state.email} onChange={this.handleChange} placeholder="Email Address" />
+                                                        </Form.Group>
+
+                                                        <Form.Group controlId="">
+                                                            <Form.Control type="password" name="password" value={this.state.password} onChange={this.handleChange} placeholder="Password" />
+                                                        </Form.Group>
+                                                    </Col>
+                                                </Row>
+                                                
+                                                <Row className="mb-2">
+                                                    <Col sm={12}>
+                                                        <Button className="btn-success btn-block" type="submit">
+                                                            Login
+                                                        </Button>
+                                                    </Col>
+                                                </Row>
+                                            </Form>
+                                        <div className="or_divider">
+                                            <span>or</span>
+                                        </div>
+
                                             
-                                            <Row className="mb-2">
-                                                <Col sm={12}>
-                                                    <Button className="btn-success btn-block" type="submit">
-                                                        Login
-                                                    </Button>
-                                                </Col>
-                                            </Row>
-                                        </Form>
-                                    <div className="or_divider">
-                                        <span>or</span>
-                                    </div>
-
-                                        
-                                    <Row>
-                                        <Col sm={12} className="mt-2">
-                                            <Button className="btn-block sign_with_google_btn">
-                                                <img src={GoogleLogo}/> Sign In With Google
+                                        <Row>
+                                            <Col sm={12} className="mt-2">
+                                                <Button className="btn-block sign_with_google_btn">
+                                                    <img src={GoogleLogo}/> Sign In With Google
+                                                </Button>
+                                            </Col>
+                                            <Col sm={12} className="mt-4">
+                                                <Button className="btn-block sign_with_fb_btn">
+                                                <img src={facebook}/> Sign In With Facebook
                                             </Button>
-                                        </Col>
-                                        <Col sm={12} className="mt-4">
-                                            <Button className="btn-block sign_with_fb_btn">
-                                            <img src={facebook}/> Sign In With Facebook
-                                        </Button>
-                                        </Col>
-                                    </Row>
-                                    
-                                    <Row className="text-center">
-                                        <Col sm={12} className="mt-4">
-                                            <NavLink to="/signup" className="" style={{color:'#1BBDF6'}}>I don't have an account</NavLink>
-                                        </Col>
-                                        <Col sm={12} className="mt-4">
-                                            <NavLink to="/" className="link_text">I Forgot My Password</NavLink>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                            </Row>
-                        </div>
-                    </Col><Col sm={4} className="my-2">
-                        <img src={family} />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-        </>
-    )
+                                            </Col>
+                                        </Row>
+                                        
+                                        <Row className="text-center">
+                                            <Col sm={12} className="mt-4">
+                                                <NavLink to="/signup" className="" style={{color:'#1BBDF6'}}>I don't have an account</NavLink>
+                                            </Col>
+                                            <Col sm={12} className="mt-4">
+                                                <NavLink to="/" className="link_text">I Forgot My Password</NavLink>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </Col><Col sm={4} className="my-2">
+                            <img src={family} />
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+            </>
+        )
+    }
 }
 export default LoginComponentCtrl;
