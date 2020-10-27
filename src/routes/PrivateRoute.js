62da5694a3a14cd282679bb5eefdb3f412c22f71
Sarvesh Chauhan <sarveshchauhan90@gmail.com>
2020-10-27 10:39:31 +0530

    update route

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
-// import { getRedirectToURL } from '../helpers/redirectHelper'
 import { get_login_token } from '../helpers/tokenHelpers';
 import RegisterPagesLayoutCtrl from '../front/layout/registerLayout';
 import LoginComponentCtrl from '../front/pages/register/Login';
@@ -12,7 +11,7 @@ export const PrivateRoute = ({ parentComponent: ParentComponent, childComponent:
         get_login_token() ?
             <ParentComponent children={ChildComponent} {...props} />
             :
-            <LoginComponentCtrl children={RegisterPagesLayoutCtrl} {...props} />
+            <RegisterPagesLayoutCtrl children={LoginComponentCtrl} {...props} />        
 
     )} />
 
