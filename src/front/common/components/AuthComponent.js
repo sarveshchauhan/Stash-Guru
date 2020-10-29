be27a91e80f9ca63eaeffef062272f4398b2152f
sanju2121 <69622591+sanju2121@users.noreply.github.com>
2020-10-29 19:11:04 +0530

    update

--- src/front/common/components/AuthComponent.js
+++ src/front/common/components/AuthComponent.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import { NavLink } from 'react-router-dom';
-import {NavDropdown} from 'react-bootstrap';
+import {NavDropdown,Nav} from 'react-bootstrap';
 import { getUsers, logoutUser } from '../../../redux';
 import { connect, useDispatch, useSelector } from 'react-redux';
+
+
+import avtar from '../../../assets/users/images/dummy/avtar.jpg';
 
 function AuthComponent(){
 
@@ -27,9 +30,24 @@ function AuthComponent(){
                 <NavLink className="nav-link login_signup_btn" to="/login">Login/SignUp</NavLink>   
             :
                 // <NavLink className="nav-link login_signup_btn" to="#" onClick={logoutHandle} >Logout</NavLink>     
-                <NavDropdown title={dispName} id="nav-dropdown">
-                    <NavDropdown.Item onClick={logoutHandle}>Logout</NavDropdown.Item>
-                </NavDropdown>
+                // <NavDropdown title={dispName} id="nav-dropdown">
+                //     <NavDropdown.Item onClick={logoutHandle}>Logout</NavDropdown.Item>
+                // </NavDropdown>
+                
+                <Nav className="ml-2 justify-content-end">
+                    <Nav.Item className="dash_avtar_user_list">
+                        <img className="dash_avtar_user" src={avtar} />
+                        <NavDropdown title={dispName}  id="collasible-nav-dropdown">
+                            <NavLink className="dropdown-item" to="/dashboard"><i className="fa fa-home" aria-hidden="true"></i> Dashboard</NavLink>
+                            <NavLink className="dropdown-item" to="/booking"><i className="fa fa-hand-pointer-o" aria-hidden="true"></i> Booking</NavLink>
+                            <NavLink className="dropdown-item" to="/listing"><i className="fa fa-map-marker" aria-hidden="true"></i> Listing</NavLink>
+                            <NavLink className="dropdown-item" to="/verification"><i className="fa fa-shield" aria-hidden="true"></i> Verification</NavLink>
+                            <NavLink className="dropdown-item" to="/profile"><i className="fa fa-user-o" aria-hidden="true"></i> Profile</NavLink>
+                            <NavLink className="dropdown-item" to="/referrals"><i className="fa fa-share-alt" aria-hidden="true"></i> Referrals</NavLink>
+                            <NavLink className="dropdown-item" to="/logout" onClick={logoutHandle}><i className="fa fa-sign-out" aria-hidden="true"></i> Logout</NavLink>
+                        </NavDropdown>
+                    </Nav.Item>
+                </Nav>
             }
                  
         </>
--- src/users/common/topbar/index.js
+++ src/users/common/topbar/index.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import {Navbar,NavDropdown, Row} from 'react-bootstrap';
+import {Navbar,NavDropdown,Nav, Row} from 'react-bootstrap';
+import './topbar.scss';
 import { NavLink } from 'react-router-dom';
 import logo from '../../../assets/front/images/colored_logo.svg';
+import avtar from '../../../assets/users/images/dummy/avtar.jpg';
 import { getUsers, logoutUser } from '../../../redux';
 import { connect, useDispatch, useSelector } from 'react-redux';
 
@@ -32,9 +34,20 @@ function UserTopbarHeaderComponent(){
                             </NavLink>
                         </div>
                         <div className="col-4 my-2">
-                        <NavDropdown title={dispName} id="nav-dropdown">
-                            <NavDropdown.Item onClick={logoutHandle}>Logout</NavDropdown.Item>
-                        </NavDropdown>
+                            <Nav className="ml-2 justify-content-end">
+                                <Nav.Item className="dash_avtar_user_list">
+                                    <img className="dash_avtar_user" src={avtar} />
+                                    <NavDropdown title={dispName}  id="collasible-nav-dropdown">
+                                        <NavLink className="dropdown-item" to="/dashboard"><i className="fa fa-home" aria-hidden="true"></i> Dashboard</NavLink>
+                                        <NavLink className="dropdown-item" to="/booking"><i className="fa fa-hand-pointer-o" aria-hidden="true"></i> Booking</NavLink>
+                                        <NavLink className="dropdown-item" to="/listing"><i className="fa fa-map-marker" aria-hidden="true"></i> Listing</NavLink>
+                                        <NavLink className="dropdown-item" to="/verification"><i className="fa fa-shield" aria-hidden="true"></i> Verification</NavLink>
+                                        <NavLink className="dropdown-item" to="/profile"><i className="fa fa-user-o" aria-hidden="true"></i> Profile</NavLink>
+                                        <NavLink className="dropdown-item" to="/referrals"><i className="fa fa-share-alt" aria-hidden="true"></i> Referrals</NavLink>
+                                        <NavLink className="dropdown-item" to="/logout" onClick={logoutHandle}><i className="fa fa-sign-out" aria-hidden="true"></i> Logout</NavLink>
+                                    </NavDropdown>
+                                </Nav.Item>
+                            </Nav>
                         </div>
                     </Row>
                 </div>
