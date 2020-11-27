19ecd003a0cd4bf24375d401b37b4aa1cca9c9a8
devopsit51 <devopsit51@gmail.com>
2020-11-27 15:37:34 +0530

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,10 @@
 export const config = {
 
     // apiUrl: 'http://localhost:5000',
-    // appUrl: 'http://localhost:3006',
+    appUrl: 'http://localhost:3006',
 
     apiUrl: 'https://sgapi.dizicraft.com',
-    appUrl: 'https://sguru.dizicraft.com',
+    // appUrl: 'https://sguru.dizicraft.com',
     socketUrl: "https://sgapi.dizicraft.com/stream",
     // socketUrl: "http://localhost:5000/stream",
 
