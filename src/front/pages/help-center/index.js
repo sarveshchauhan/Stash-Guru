f7a5db1a35b7caf1bf1b1c2e845841dfdb92d424
sanju2121 <69622591+sanju2121@users.noreply.github.com>
2020-12-03 19:03:31 +0530

    update

--- src/front/pages/help-center/index.js
+++ src/front/pages/help-center/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Col, Container, Row, Button, Form, Nav, Table } from 'react-bootstrap';
+import { Col, Container, Row, Button, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import SearchComponent from '../../common/components/SearchCompo';
 
@@ -7,7 +7,7 @@ import banner_img from '../../../assets/front/images/img/about_banner_img.png';
 import helpCenter from '../../../assets/front/images/img/helpCenter.png'
 import star from '../../../assets/front/images/icons/star.png';
 import avlSpace from '../../../assets/front/images/icons/avlSpace.png';
-import calender from '../../../assets/front/images/icons/calender.png';
+import calendar_img from '../../../assets/front/images/img/calendar_img.png';
 
 function FrontHelpCenterCtrl(){
     return(
@@ -15,15 +15,18 @@ function FrontHelpCenterCtrl(){
             <section className="page_banner about_page_banner">
                 <Container>
                     <Row>
-                        <Col lg={6}>
-                            <div className="page_banner_content">
+                        <Col lg={7}>
+                            <div className="help_center_banner_btns">
                                 <h2>Help Center</h2>
-                                <p>We’d love to hear from you!</p>
-                                <div className="mt-5">
-                                </div>
+                                <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+                                    <ToggleButton value={1}>Booking a Space</ToggleButton>
+                                    <ToggleButton value={2}>Hosting a Space</ToggleButton>
+                                    <ToggleButton value={3}>Common Questions</ToggleButton>
+                                    <ToggleButton value={4}>Covid-19</ToggleButton>
+                                </ToggleButtonGroup>
                             </div>
                         </Col>
-                        <Col lg={6}>
+                        <Col lg={5}>
                             <div className="page_banner_img">
                                 <img src={helpCenter} alt="" />
                             </div>
@@ -33,20 +36,54 @@ function FrontHelpCenterCtrl(){
             </section>
 
             
-            <section>
+            <section className="bg-white py-5">
                 <Container>
                     <Row>
                         <Col md="4">
-                            <h5 className="text5">Manage Your Booking</h5>
-                            <ul>
-                                <li>Making an Enquiry</li>
-                                <li>Making a Booking</li>
-                                <li>Verifying Your Account</li>
-                                <li>Guest Cancellations & Refunds</li>
-                            </ul>
+                            <div className="help_center_box_card">
+                                <div className="help_center_box_card_img">
+                                    <img src={calendar_img} alt="" />
+                                </div>
+                                <div className="help_center_box_card_body">
+                                    <h5 className="text5">Manage Your Booking</h5>
+                                    <ul>
+                                        <li>Making an Enquiry</li>
+                                        <li>Making a Booking</li>
+                                        <li>Verifying Your Account</li>
+                                        <li>Guest Cancellations & Refunds</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </Col>
-                        <Col md="4"></Col>
-                        <Col md="4"></Col>
+                        <Col md="4">
+                            <div className="help_center_box_card">
+                                <div className="help_center_box_card_img">
+                                    <img src={calendar_img} alt="" />
+                                </div>
+                                <div className="help_center_box_card_body">
+                                    <h5 className="text5">Protecting Your Booking</h5>
+                                    <ul>
+                                        <li>Booking Protection</li>
+                                        <li>Payment Protection</li>
+                                        <li>Putting Down a Deposit</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col md="4">
+                            <div className="help_center_box_card">
+                                <div className="help_center_box_card_img">
+                                    <img src={calendar_img} alt="" />
+                                </div>
+                                <div className="help_center_box_card_body">
+                                    <h5 className="text5">Ending a Booking</h5>
+                                    <ul>
+                                        <li>Serving Notice</li>
+                                        <li>Moving Out</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </Col>
                     </Row>
                 </Container>
             </section>
