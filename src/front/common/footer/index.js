1b5d1a0399e9c29fd4b0063db7dc51378a5ff9d7
devopsit51 <devopsit51@gmail.com>
2020-12-05 10:57:09 +0530

    update

--- src/front/common/footer/index.js
+++ src/front/common/footer/index.js
@@ -57,7 +57,7 @@ function FooterComponent() {
                                         <Nav className="flex-column">
                                             <NavLink to="" className="nav-link">Start Storing</NavLink>
                                             <NavLink to="" className="nav-link">Domestic Storage</NavLink>
-                                            <NavLink to="" className="nav-link">Business Storage</NavLink>
+                                            <NavLink to="/business-storage" className="nav-link">Business Storage</NavLink>
                                             <NavLink to="" className="nav-link">Rent A Parking Space</NavLink>
                                             <NavLink to="" className="nav-link">Storage Space In London</NavLink>
                                             <NavLink to="" className="nav-link">Rent A Garage</NavLink>
@@ -85,7 +85,7 @@ function FooterComponent() {
                                             <NavLink to="/" className="nav-link">Home</NavLink>
                                             <NavLink to="/about" className="nav-link">About</NavLink>
                                             <NavLink to="/FAQS" className="nav-link">FAQs</NavLink>
-                                            <NavLink to="/help-center" className="nav-link">Help Centre</NavLink>
+                                            <NavLink to="/manage-your-booking" className="nav-link">Help Centre</NavLink>
                                             <NavLink to="/refund-policy" className="nav-link">Cancellations & Refunds Policy</NavLink>
                                             <NavLink to="/sitemap" className="nav-link">Sitemap</NavLink>
                                         </Nav>
@@ -105,7 +105,7 @@ function FooterComponent() {
                                                 <Nav className="flex-column">
                                                     <NavLink to="" className="nav-link">Start Storing</NavLink>
                                                     <NavLink to="" className="nav-link">Domestic Storage</NavLink>
-                                                    <NavLink to="" className="nav-link">Business Storage</NavLink>
+                                                    <NavLink to="/business-storage" className="nav-link">Business Storage</NavLink>
                                                     <NavLink to="" className="nav-link">Rent A Parking Space</NavLink>
                                                     <NavLink to="" className="nav-link">Storage Space In London</NavLink>
                                                     <NavLink to="" className="nav-link">Rent A Garage</NavLink>
@@ -145,7 +145,7 @@ function FooterComponent() {
                                                     <NavLink to="/" className="nav-link">Home</NavLink>
                                                     <NavLink to="/about" className="nav-link">About</NavLink>
                                                     <NavLink to="/FAQS" className="nav-link">FAQs</NavLink>
-                                                    <NavLink to="/help-center" className="nav-link">Help Centre</NavLink>
+                                                    <NavLink to="/manage-your-booking" className="nav-link">Help Centre</NavLink>
                                                     <NavLink to="/refund-policy" className="nav-link">Cancellations & Refunds Policy</NavLink>
                                                     <NavLink to="/sitemap" className="nav-link">Sitemap</NavLink>
                                                 </Nav>
@@ -173,136 +173,10 @@ function FooterComponent() {
                                     <NavLink className="nav-link" to="/">Privacy & Policy</NavLink>
                                 </Nav.Item>
                             </Nav>
-                        </div>
-                    </Col>
-                    <Col lg={8}>
-                        <div className="hidden_sm_ftr_menu">
-                            <Row className="justify-content-between">
-                                <Col sm={4} className="footer_links">
-                                    <h4 className="mt-3">Rent A Space</h4>
-                                    <Nav className="flex-column">
-                                        <NavLink to="" className="nav-link">Start Storing</NavLink>
-                                        <NavLink to="" className="nav-link">Domestic Storage</NavLink>
-                                        <NavLink to="/business-storage" className="nav-link">Business Storage</NavLink>
-                                        <NavLink to="" className="nav-link">Rent A Parking Space</NavLink>
-                                        <NavLink to="" className="nav-link">Storage Space In London</NavLink>
-                                        <NavLink to="" className="nav-link">Rent A Garage</NavLink>
-                                        <NavLink to="" className="nav-link">Rent A Warehouse</NavLink>
-                                        <NavLink to="" className="nav-link">Rent A Spare Room</NavLink>
-                                    </Nav>
-                                </Col>
-                                <Col sm={4} className="footer_links">
-                                    <h4 className="mt-3">Hosting</h4>
-                                    <Nav className="flex-column">
-                                        <NavLink to="/become-a-host" className="nav-link">Become A Host</NavLink>
-                                        <NavLink to="" className="nav-link">Rent Your Garage</NavLink>
-                                        <NavLink to="" className="nav-link">Rent Your Parking Space</NavLink>
-                                        <NavLink to="" className="nav-link">Rent Your Warehouse</NavLink>
-                                        <NavLink to="" className="nav-link">Rent Your Spare Room</NavLink>
-                                        <NavLink to="" className="nav-link">Rent Your Outhouse</NavLink>
-                                        <NavLink to="" className="nav-link">Rent Your Lock-Up</NavLink>
-                                        <NavLink to="" className="nav-link">Rent Your Loft</NavLink>
-                                        <NavLink to="" className="nav-link">Rent Your Basement</NavLink>
-                                    </Nav>
-                                </Col>
-                                <Col sm={4} className="footer_links">
-                                    <h4 className="mt-3">Platform</h4>
-                                    <Nav className="flex-column">
-                                        <NavLink to="/" className="nav-link">Home</NavLink>
-                                        <NavLink to="/about" className="nav-link">About</NavLink>
-                                        <NavLink to="/FAQS" className="nav-link">FAQs</NavLink>
-                                        <NavLink to="/manage-your-booking" className="nav-link">Help Centre</NavLink>
-                                        <NavLink to="/refund-policy" className="nav-link">Cancellations & Refunds Policy</NavLink>
-                                        <NavLink to="/sitemap" className="nav-link">Sitemap</NavLink>
-                                    </Nav>
-                                </Col>
-                            </Row>
-                        </div>
-
-
-                        <div className="hidden_lg_ftr_menu">
-                            <Row>
-                                <Col sm={12} className="footer_links">
-                                    <Accordion>
-                                        <Accordion.Toggle variant="link" eventKey="0">
-                                            <h4 className="my-2">Rent A Space</h4>
-                                        </Accordion.Toggle>
-                                        <Accordion.Collapse eventKey="0">
-                                            <Nav className="flex-column">
-                                                <NavLink to="" className="nav-link">Start Storing</NavLink>
-                                                <NavLink to="" className="nav-link">Domestic Storage</NavLink>
-                                                <NavLink to="/business-storage" className="nav-link">Business Storage</NavLink>
-                                                <NavLink to="" className="nav-link">Rent A Parking Space</NavLink>
-                                                <NavLink to="" className="nav-link">Storage Space In London</NavLink>
-                                                <NavLink to="" className="nav-link">Rent A Garage</NavLink>
-                                                <NavLink to="" className="nav-link">Rent A Warehouse</NavLink>
-                                                <NavLink to="" className="nav-link">Rent A Spare Room</NavLink>
-                                            </Nav>
-                                        </Accordion.Collapse>
-                                    </Accordion>
-                                </Col>
-                                <Col sm={12} className="footer_links">
-                                    <Accordion>
-                                        <Accordion.Toggle variant="link" eventKey="1">
-                                            <h4 className="my-2">Hosting</h4>
-                                        </Accordion.Toggle>
-                                        <Accordion.Collapse eventKey="1">
-                                            <Nav className="flex-column">
-                                                <NavLink to="/become-a-host" className="nav-link">Become A Host</NavLink>
-                                                <NavLink to="" className="nav-link">Rent Your Garage</NavLink>
-                                                <NavLink to="" className="nav-link">Rent Your Parking Space</NavLink>
-                                                <NavLink to="" className="nav-link">Rent Your Warehouse</NavLink>
-                                                <NavLink to="" className="nav-link">Rent Your Spare Room</NavLink>
-                                                <NavLink to="" className="nav-link">Rent Your Outhouse</NavLink>
-                                                <NavLink to="" className="nav-link">Rent Your Lock-Up</NavLink>
-                                                <NavLink to="" className="nav-link">Rent Your Loft</NavLink>
-                                                <NavLink to="" className="nav-link">Rent Your Basement</NavLink>
-                                            </Nav>
-                                        </Accordion.Collapse>
-                                    </Accordion>
-                                </Col>
-                                <Col sm={12} className="footer_links">
-                                    <Accordion>
-                                        <Accordion.Toggle variant="link" eventKey="2">
-                                            <h4 className="my-2">Platform</h4>
-                                        </Accordion.Toggle>
-                                        <Accordion.Collapse eventKey="2">
-                                            <Nav className="flex-column">
-                                                <NavLink to="/" className="nav-link">Home</NavLink>
-                                                <NavLink to="/about" className="nav-link">About</NavLink>
-                                                <NavLink to="/FAQS" className="nav-link">FAQs</NavLink>
-                                                <NavLink to="/manage-your-booking" className="nav-link">Help Centre</NavLink>
-                                                <NavLink to="/refund-policy" className="nav-link">Cancellations & Refunds Policy</NavLink>
-                                                <NavLink to="/sitemap" className="nav-link">Sitemap</NavLink>
-                                            </Nav>
-                                        </Accordion.Collapse>
-                                    </Accordion>
-                                </Col>
-                            </Row>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-
-            <Container className="copyright_section">
-                <hr />
-                <Row className="align-items-center">
-                    <Col lg={6} className="text-center text-lg-left">
-                        Copyright © stash.guru 2018. All Rights Reserved.
-                    </Col>
-                    <Col lg={6}>
-                        <Nav className="justify-content-center justify-content-lg-end">
-                            <Nav.Item as="li">
-                                <NavLink className="nav-link" to="/">Terms of use</NavLink>
-                            </Nav.Item>
-                            <Nav.Item as="li">
-                                <NavLink className="nav-link" to="/">Privacy & Polic</NavLink>
-                            </Nav.Item>
-                        </Nav>
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
         </>
     )
 }
