5b184b244a36d7bf344eed8c54436de6c9d85f3d
Sanjeev <thekingman573@gmail.com>
2021-02-18 18:28:14 +0530

    update non-descrimination pages

--- src/front/pages/non-descrimination/index.js
+++ src/front/pages/non-descrimination/index.js
@@ -11,16 +11,22 @@ function NonDiscriminationPolicyCtrl() {
 
   return (
     <>
-      <section>
+    
+    <section className="section_padding bg-success">
+      <Container fluid className="text-center">
+        <h1>{t("nonDiscriminationTitle")}</h1>
+      </Container>
+    </section>
+
+      <section className="section_padding">
         <Container>
-          <h1>{t("nonDiscriminationTitle")}</h1>
           <p>
             Utilizatorii noștri provin din mai multe medii și ne așteptăm să se
             trateze reciproc în conformitate cu legea, cu respect și fără
             discriminare.
           </p>
           <p>Dumneavoastră nu puteti:</p>
-          <ul>
+          <ul className="pl-3">
             <li>
               refuza de a incheia un contract cu un utilizator pe baza rasei,
               culorii, etniei, originii naționale, religiei, orientării sexuale,
@@ -38,20 +44,16 @@ function NonDiscriminationPolicyCtrl() {
               orientării sexuale, identității de gen, vârstei sau stării civile.
             </li>
           </ul>
-          <p>
-            <strong>Identitatea sexuala</strong>
-          </p>
+          <h4>Identitatea sexuala</h4>
           <p>
             Considerăm că genul unei persoane este ceea ce indica în profilul
             său de utilizator. O gazdă poate pune la dispoziția oaspeților un
             spațiu de stocare bazat pe sexul gazdei și nu pentru altul, în cazul
             în care acesta se bazează pe siguranță și / sau securitate.
           </p>
-          <p>
-            <strong>Dizabilitate</strong>
-          </p>
+          <h4>Dizabilitate</h4>
           <p>Gazdele nu pot:</p>
-          <ul>
+          <ul className="pl-3">
             <li>
               Refuza un chirias pe baza oricărui handicap real sau perceput.
             </li>
